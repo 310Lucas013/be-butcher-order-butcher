3fc6b85bf9f81c84fb331b36def3fc4fd44601e3
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-04-12 14:25:25 +0200

    customer creation with rabbitmq works.

--- src/main/java/com/lucas/butcher/controllers/ButcherController.java
+++ src/main/java/com/lucas/butcher/controllers/ButcherController.java
@@ -5,9 +5,10 @@ import com.lucas.butcher.services.OpenTimeService;
 import com.lucas.butcher.services.ScheduleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin(origins = {"http://localhost:6001"})
+@RequestMapping(value = "/butchers")
 public class ButcherController {
     private final ButcherService butcherService;
     private final OpenTimeService openTimeService;
