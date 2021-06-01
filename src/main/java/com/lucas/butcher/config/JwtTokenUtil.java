a89c443b0365e38707d071abec1c48c32ae52fbd
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-06-01 15:52:33 +0200

    set up authentication for butcher service

---
+++ src/main/java/com/lucas/butcher/config/JwtTokenUtil.java
@@ -0,0 +1,78 @@
+package com.lucas.butcher.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil implements Serializable {
+
+    private static final long serialVersionUID = -2550185165626007488L;
+
+    public static final long JWT_TOKEN_VALIDITY = 60 * 60 * 10;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public String getTypeFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("type", String.class);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    private Boolean ignoreTokenExpiration(String token) {
+        // here you specify tokens, for that the expiration is ignored
+        return false;
+    }
+
+    public Boolean canTokenBeRefreshed(String token) {
+        return (!isTokenExpired(token) || ignoreTokenExpiration(token));
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        System.out.println("In ValidateToken, past UsernameFromToken");
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+//        if (!isTokenExpired(token)) {
+//                String type = getTypeFromToken(token);
+//                System.out.println(type);
+////            UserDetails userDetails
+//
+////            GrantedAuthority authority = new SimpleGrantedAuthority(user.getType().name());
+////            return new org.springframework.security.core.userdetails.User(getUsernameFromToken(token), user.getPassword(),
+////                    Collections.singletonList(authority))
+//        }
+//        System.out.println("returns");
+//        return (!isTokenExpired(token));
+    }
+}
